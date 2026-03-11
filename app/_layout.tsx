f39e46fb7fe6f722f56779d862b701187e33ba33
Allan Gabriel <allans23211467-2@alunos.unicesumar.edu.br>
2026-03-11 16:22:18 -0300

    Add Post component and update package-lock.json for dev dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-
   return (
     <SafeAreaView>
-      
+      <Text>Test</Text>
     </SafeAreaView>
   );
 }
---
+++ app/post.tsx
@@ -0,0 +1,13 @@
+import { Image, View } from "react-native";
+
+const Post = () => {
+  return (
+    <View>
+      <View>
+        <Image source={}></Image>
+      </View>
+    </View>
+  );
+};
+
+export default Post;
